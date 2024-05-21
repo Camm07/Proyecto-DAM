d8c469321575bf12cd1bb48482c116a7bd097300
Camm07 <127072716+Camm07@users.noreply.github.com>
2024-05-21 11:08:43 -0700

    sqflite y firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp( MaterialApp(
     debugShowCheckedModeBanner: false,
     home: MyApp(),
   ));
 }
+
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
